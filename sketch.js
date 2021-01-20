fac74a9180eb4c0264b3dfddffa7038ef6071da6
suravimondal <73650143+suravimondal@users.noreply.github.com>
2021-01-20 16:42:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+
+var engine, world, ground;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine= Engine.create();
+  world= engine.world;
+  
+  var options={
+    isStatic:true
+  }
+    
+    
+  ground= Bodies.rectangle(200,380,400,20, options);
+  World.add(world,ground);
+  console.log(ground);
+
+}
+
+function draw() {
+  background(255);
+    Engine.update(engine);
+rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20);
+
+  drawSprites();
+}
